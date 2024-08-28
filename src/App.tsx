9b7080cab5d54e8a4b30ecdb0ada372ae81938d5
femi-venus <femivenus@gmail.com>
2024-08-28 17:42:46 +0530

    card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Graphs from './components/Graphs';
 
+import Component from './components/Cards';
+
 
 function App() {
   return (
     <div className="App">
-     <Graphs />
+     <Component />
     </div>
   );
 }
--- src/components/config.ts
+++ src/components/config.ts
@@ -1,27 +1,82 @@
 export const config = {
-   years:[{id:2022,value:"2022"},{id:2023,value:"2023"},{id:2024,value:"2024"}],
-    tabs: [
-      {
-        id: 1,
-        label: "Total active users",
-        value: "18,765",
-      },
-      {
-        id: 2,
-        label: "Total installed",
-        value: "4,876",
-      },
-      {
-        id: 3,
-        label: "Total downloads",
-        value: "678",
-      },
-    ],
-    pieData: [
-      { id: 1, label: "Mac", value: "12,244" },
-      { id: 2, label: "Windows", value: "53,345" },
-      { id: 3, label: "iOS", value: "44,313" },
-      { id: 4, label: "Android", value: "78,343" },
-    ],
-    months:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
-}
+  cards: [
+    {
+      id: 1,
+      title: "The Future of Renewable Energy: Innovations and Challenges Ahead",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      date: "20 Aug 2024",
+      status: "Draft",
+      image:
+        "https://media.licdn.com/dms/image/v2/D4D12AQGnGV1ivJdjRA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1698442116147?e=2147483647&v=beta&t=q_1WvwKUogAUFb9n3kQAVKOF3zN8fqLCCSeEGb9y6g0",
+      avatar:
+        "https://api-prod-minimal-v610.pages.dev/assets/images/avatar/avatar-1.webp",
+      label: "Jason Joy",
+      email:"asdf",
+      views:1950,
+      shares:9910,
+      comments:9120,
+    },
+    {
+      id: 2,
+      title:
+        "Exploring the Impact of Artificial Intelligence on Modern Healthcare",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      date: "18 Aug 2024",
+      status: "Published",
+      image:
+        "https://api-prod-minimal-v610.pages.dev/assets/images/cover/cover-2.webp",
+      avatar:
+        "https://api-prod-minimal-v610.pages.dev/assets/images/avatar/avatar-2.webp",
+      label: "Harry Smith",
+      email:"asdff",
+      views:9120,
+      shares:1950,
+      comments:6980,
+    },
+  ],
+
+  years: [
+    { id: 2022, value: "2022" },
+    { id: 2023, value: "2023" },
+    { id: 2024, value: "2024" },
+  ],
+  tabs: [
+    {
+      id: 1,
+      label: "Total active users",
+      value: "18,765",
+    },
+    {
+      id: 2,
+      label: "Total installed",
+      value: "4,876",
+    },
+    {
+      id: 3,
+      label: "Total downloads",
+      value: "678",
+    },
+  ],
+  pieData: [
+    { id: 1, label: "Mac", value: "12,244" },
+    { id: 2, label: "Windows", value: "53,345" },
+    { id: 3, label: "iOS", value: "44,313" },
+    { id: 4, label: "Android", value: "78,343" },
+  ],
+  months: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+};
